84ef1c63f854bfda96382728d16aa1231c1e5ca6
capraibex <daniel.steinboeck@gmx.at>
2022-12-11 22:19:43 +0100

    refactor solution of day 9

--- 9/9.ts
+++ 9/9.ts
@@ -1,33 +1,61 @@
 const newLine = '\r\n';
 const input = await Deno.readTextFile('./input.txt');
 
-type coord = [number, number];
+class Coordinate {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(coord: Coordinate): Coordinate {
+    this.x += coord.x;
+    this.y += coord.y;
+    return this;
+  }
+
+  subtract(coord: Coordinate): Coordinate {
+    this.x -= coord.x;
+    this.y -= coord.y;
+    return this;
+  }
+
+  copy(): Coordinate {
+    return new Coordinate(this.x, this.y);
+  }
+
+  toString(): string {
+    return [this.x, this.y].join();
+  }
+}
 
 function solve(part: number) {
   const ropeLength = part === 1 ? 2 : 10;
-  const rope: coord[] = [...new Array(ropeLength)].map((_) => [0, 0]);
-  const coords: Set<string> = new Set([[0, 0].join()]);
-  const directions: { [key: string]: coord } = {
-    U: [1, 1],
-    R: [1, 0],
-    D: [-1, 1],
-    L: [-1, 0],
+  const rope: Coordinate[] = [...new Array(ropeLength)].map(() => new Coordinate(0, 0));
+  const coords: Set<string> = new Set([rope[0].toString()]);
+  const directions: { [key: string]: Coordinate } = {
+    U: new Coordinate(0, 1),
+    R: new Coordinate(1, 0),
+    D: new Coordinate(0, -1),
+    L: new Coordinate(-1, 0),
   };
 
   input
     .split(newLine)
     .map((row) => row.split(' '))
     .forEach(([direction, n]) => {
-      [...new Array(+n)].forEach(() => {
-        rope[0][directions[direction][1]] += directions[direction][0];
+      [...new Array(Number(n))].forEach(() => {
+        rope[0].add(directions[direction]);
 
-        for (let j = 0; j < rope.length - 1; j++) {
-          const coordsDiff = rope[j].map((c, i) => c - rope[j + 1][i]);
+        for (let i = 0; i < rope.length - 1; i++) {
+          const { x: dx, y: dy } = rope[i].copy().subtract(rope[i + 1]);
 
-          if (coordsDiff.some((c) => Math.abs(c) >= 2)) {
-            const diffDir = coordsDiff.map((c) => Math.sign(c));
-            rope[j + 1] = rope[j + 1].map((c, i) => c + diffDir[i]) as coord;
-            coords.add(rope[rope.length - 1].join());
+          if (Math.hypot(dx, dy) >= 2) {
+            const dCoord = new Coordinate(...([dx, dy].map(Math.sign) as [number, number]));
+            rope[i + 1].add(dCoord);
+            coords.add(rope[rope.length - 1].toString());
           }
         }
       });
